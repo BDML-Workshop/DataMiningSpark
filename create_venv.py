c0c52c8b674db8730cfdd0bfc55f2c61644efd3b
Pascal Fares <pascal.fares@cofares.net>
2020-10-20 22:06:35 +0300

    dependencies packages

--- create_venv.py
+++ create_venv.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 import os, subprocess, sys
 from pathlib import Path
 import logging
@@ -17,7 +19,13 @@ if not os.path.exists(VENV_PATH):
     logger.info(f"Creating virtual env {str(VENV_PATH)}")
     subprocess.check_call([sys.executable, "-m", "venv", str(VENV_PATH)])
 else:
-    logger.info(f"Cvirtual env {str(VENV_PATH)} already exist")
+    logger.info(f"virtual env {str(VENV_PATH)} already exist")
+
+logger.info(f"Step 1.2: tuning Virtual env, instaling needed packages")
+python_v_path = str(VENV_PATH)+'/bin/python'
+subprocess.check_call([python_v_path, "-m", "pip", 'install', '--upgrade', 'pip', 'setuptools'])
+subprocess.check_call([python_v_path, "-m", "pip", 'install', 'jupyterlab', 'findspark', 'requests'])
+
 
 logger.info(f"Step 2: activating the virtual environnement")
 
